f9c99af412a4ca8bab488b7c7f3a27a7d5c8354f
Tanahashi-github <hikari.t.48@gmail.com>
2024-06-25 00:16:51 +0900

    課題011を追加

---
+++ kadai_011/foreach.php
@@ -0,0 +1,26 @@
+<!DOCTYP html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>課題011</title>
+</head>
+
+<body>
+    <p>
+        <?php
+        $product_data = [
+            '名前' => '玉ねぎ',
+            '値段' => 200,
+            '産地' => '北海道' 
+        ];
+
+        foreach ($product_data as $key => $value) {
+            echo "{$key} : {$value}<br>";
+        }
+
+        ?>
+    </p>
+</body>
+
+</html>
